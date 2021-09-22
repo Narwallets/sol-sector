c90b1dd85d2614381fa8e4ffbccce28ca7243812
Martin Ruiz <arcollector@gmail.com>
2021-09-21 21:37:15 -0300

    feat: use page component in marketplace page

--- src/Marketplace/containers/Marketplace.tsx
+++ src/Marketplace/containers/Marketplace.tsx
@@ -1,38 +1,41 @@
 import React from 'react';
 import { Flex, Link, Center, Button, Spacer, Box } from '@chakra-ui/react'
+import { Page } from '../../shared/components'
 
 export function Marketplace() {
   return (
-    <Flex
-      direction="column"
-      width={500}
-      height={220}
-      backgroundColor="#ccc"
-    >
+    <Page id="marketplace">
       <Flex
-        direction="row"
+        direction="column"
+        width={500}
+        height={220}
+        backgroundColor="#ccc"
       >
-        <Box backgroundColor="#f00" width={100} height={100}>
-          <img src="" width={100} height={90}></img>
-          <span style={{fontSize:10}}>16</span>
-        </Box>
-        <Box backgroundColor="#f0f" width={100} height={100} />
-        <Box backgroundColor="#0ff" width={100} height={100} />
-        <Box backgroundColor="#ff0" width={100} height={100} />
-        <Box backgroundColor="#00f" width={100} height={100} />
-      </Flex>
+        <Flex
+          direction="row"
+        >
+          <Box backgroundColor="#f00" width={100} height={100}>
+            <img src="" width={100} height={90}></img>
+            <span style={{fontSize:10}}>16</span>
+          </Box>
+          <Box backgroundColor="#f0f" width={100} height={100} />
+          <Box backgroundColor="#0ff" width={100} height={100} />
+          <Box backgroundColor="#ff0" width={100} height={100} />
+          <Box backgroundColor="#00f" width={100} height={100} />
+        </Flex>
 
-      <Spacer />
-
-      <Flex
-        direction="row"
-      >
-        <Box backgroundColor="#f00" width={100} height={100} />
-        <Box backgroundColor="#f0f" width={100} height={100} />
         <Spacer />
-        <Box backgroundColor="#00f" width={100} height={100} />
+
+        <Flex
+          direction="row"
+        >
+          <Box backgroundColor="#f00" width={100} height={100} />
+          <Box backgroundColor="#f0f" width={100} height={100} />
+          <Spacer />
+          <Box backgroundColor="#00f" width={100} height={100} />
+        </Flex>
       </Flex>
-    </Flex>
+    </Page>
   );
 }
 
